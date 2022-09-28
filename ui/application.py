84dfbbb81df8c123eeba253060330c7a581714ab
erlete <dev.szblzpaulo@gmail.com>
2022-09-28 11:56:32 +0200

    Distribute and update windows
    Fragmented the application in three windows: the main window, the information and the text input one.
    
    This improves readability and overall code quality while also reducing future implementations' complexity.

--- ui/application.py
+++ ui/application.py
@@ -1,12 +1,15 @@
-import sys, os
+import sys
+import os
 
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 from utils.parser import PseudoCodeParser
+from ui.labels import *
 
 import subprocess
 from io import StringIO
+
 
 class TextInputWindow(QMainWindow):
 
@@ -14,7 +17,8 @@ class TextInputWindow(QMainWindow):
         "title": "Pseudo Code Parser · Text Mode",
         "clear_button": "Clear input",
         "exec_button": "Execute code",
-        "code_output": "Code execution output:\n\n"
+        "code_output": "Code execution output:\n\n",
+        "error_output": "Code execution status:\n\n"
     }
 
     def __init__(self, main_window):
@@ -35,24 +39,30 @@ class TextInputWindow(QMainWindow):
         # Control buttons:
         self.clear_button = QPushButton(self.HEADERS.get("clear_button"))
         self.clear_button.clicked.connect(self.code_input.clear)
+        self.clear_button.clicked.connect(self.code_output.clear)
 
         self.execute_button = QPushButton(self.HEADERS.get("exec_button"))
         self.execute_button.clicked.connect(self.execute_code)
 
         # Execution output field:
         self.exec_output = QLabel(self.HEADERS.get("code_output"))
+        self.exec_error = QLabel(self.HEADERS.get("error_output"))
 
         # Layout settings:
         self.input_area = QHBoxLayout()
+        self.output_area = QHBoxLayout()
         self.window_area = QVBoxLayout()
 
         self.input_area.addWidget(self.code_input)
         self.input_area.addWidget(self.code_output)
 
+        self.output_area.addWidget(self.exec_output)
+        self.output_area.addWidget(self.exec_error)
+
         self.window_area.addWidget(self.clear_button)
         self.window_area.addWidget(self.execute_button)
         self.window_area.addLayout(self.input_area)
-        self.window_area.addWidget(self.exec_output)
+        self.window_area.addLayout(self.output_area)
 
         # Main widget settings:
         self.widget.setLayout(self.window_area)
@@ -73,6 +83,7 @@ class TextInputWindow(QMainWindow):
         tmp = sys.stdout
         sys.stdout = redirect = StringIO()
         code_string = self.code_output.toPlainText()
+        error_string = "OK"
 
         try:
             # If there is code to execute:
@@ -84,7 +95,8 @@ class TextInputWindow(QMainWindow):
                 exec_string = "No executable code found."
 
         except Exception as exception:
-            exec_string = f"Error: {exception}"
+            exec_string = ''
+            error_string = f"Error: {exception}"
 
         finally:
             # Standard output restoration:
@@ -96,9 +108,15 @@ class TextInputWindow(QMainWindow):
                 + exec_string
             )
 
+            self.exec_error.setText(
+                self.HEADERS.get("error_output")
+                + error_string
+            )
+
     def closeEvent(self, event):
         self.main_window.reset_layout(event)
         event.accept()
+
 
 class InformationWindow(QMainWindow):
 
@@ -111,12 +129,57 @@ class InformationWindow(QMainWindow):
         self.main_window = main_window
         self.setWindowTitle(self.HEADERS.get("title"))
 
+        widget = QWidget()
+        layout = QVBoxLayout()
+
+        # Information text:
+        header_1 = Title("Pseudo Code Parser")
+        paragraph_1 = Subtitle("""The parser is a simple tool that allows you to write pseudo code and execute
+it in a Python environment. The program will parse the code and convert it
+into Python code so that it can be executed.""")
+
+        header_2 = Title("Usage")
+        paragraph_2 = Subtitle("""The parser is divided into two modes: text mode and image mode.
+
+In text mode, you can write pseudo code in the text area on the left and
+execute it using the button that says "Execute code", located on top of the
+window. You can also clear the input using the button that says "Clear input".
+The output of the code execution will be displayed on the bottom side of the
+window.
+
+Furthermore, you might notice that the text area on the right displays updated
+Python code as you write pseudo code. This is the parsed code, which is the
+code that will be executed when you press the "Execute code" button. If, by
+any change, the execution returns an error code due to a parsing feature that
+is not yet implemented, you can directly edit the code in the right text area,
+fix the issue and execute it.
+
+Image mode is yet to be implemented. Stay tuned!""")
+
+        layout.addWidget(header_1)
+        layout.addWidget(paragraph_1)
+        layout.addWidget(header_2)
+        layout.addWidget(paragraph_2)
+
+        padding = QHBoxLayout()
+
+        padding.addWidget(QWidget())
+        padding.addLayout(layout)
+        padding.addWidget(QWidget())
+
+        widget.setLayout(padding)
+
+        self.setCentralWidget(QScrollArea())
+        self.centralWidget().setWidgetResizable(False)
+        self.centralWidget().setWidget(widget)
+
         screen = QScreen.geometry(QApplication.primaryScreen())
-        self.resize(screen.width() * 0.8, screen.height() * .8)
+        self.resize(screen.width() * .6, screen.height() * .5)
 
     def closeEvent(self, event):
         self.main_window.reset_layout(event)
         event.accept()
+
 
 class MainWindow(QMainWindow):
 
@@ -132,9 +195,15 @@ class MainWindow(QMainWindow):
         self.window_1 = InformationWindow(self)
         self.window_2 = TextInputWindow(self)
 
-        header = QLabel("Title")
-        header.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        header.setFont(QFont("Monaco", 30))
+        header_1 = Title(self)
+        header_1.setPixmap(QPixmap("media/logo_text_1.png"))
+        header_1.setScaledContents(True)
+
+        header_2 = Subtitle(
+            "The ultimate solution for students"
+            + "\nstruggling to understand how"
+            + "\npseudo code works"
+        )
 
         self.info_button = QPushButton("How to use the parser?")
         self.text_input_button = QPushButton("Text input")
@@ -142,9 +211,12 @@ class MainWindow(QMainWindow):
         self.info_button.clicked.connect(self.set_info_screen)
         self.text_input_button.clicked.connect(self.set_text_input_screen)
 
-        footer = QLabel("Footer")
-        footer.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        footer.setFont(QFont("Monaco", 30))
+        footer = Footer(
+            "For more information about my work, please visit"
+            + "\nhttps://github.com/erlete"
+            + "\n\nHope you like it! :)"
+            + "\n\n© erlete, 2022, All Rights Reserved"
+        )
 
         # create main widget
         self.widget = QWidget()
@@ -161,23 +233,28 @@ class MainWindow(QMainWindow):
         self.padding = QHBoxLayout()
 
         self.window_area = QVBoxLayout()
-        self.window_area.addWidget(header)
+        self.window_area.addWidget(header_1)
+        self.window_area.addWidget(header_2)
         self.window_area.addWidget(self.info_button)
         self.window_area.addWidget(self.text_input_button)
         self.window_area.addWidget(footer)
-        #self.window_area.addLayout(self.stacked_layout)
+
+        MARGINS = (30, 30, 30, 30)
+        self.window_area.setContentsMargins(*MARGINS)
 
         self.padding.addWidget(QWidget())
         self.padding.addLayout(self.window_area)
         self.padding.addWidget(QWidget())
 
-        #self.widget.setLayout(self.window_area)
+        # self.widget.setLayout(self.window_area)
         self.widget.setLayout(self.padding)
         self.setCentralWidget(self.widget)
 
         # Screen resizing:
         screen = QScreen.geometry(QApplication.primaryScreen())
         self.resize(screen.width() * 0.4, screen.height() * .6)
+        # prevent resizing
+        self.setFixedSize(self.size())
 
     def set_info_screen(self):
         self.stacked_layout.setCurrentIndex(1)
