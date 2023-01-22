dc2535c16311a6c2402a8857cd9ace62cf1901e1
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-01-22 18:28:34 +0100

    Update samples and validations

--- src/tests/validations/do_while_loop/sample_2.py
+++ src/tests/validations/do_while_loop/sample_2.py
@@ -1,19 +1,24 @@
 def main():
     x = 0
     y = 0
-
     x = x + 1
     y = y + 1
-
-    while y != 10:
+    print(x, y)
+    while y < 10:
         y = y + 1
+        print(x, y)
+    while x < 10:
+        print(x)
 
     while x < 10:
         x = x + 1
         y = y + 1
-
-        while y != 10:
+        print(x, y)
+        while y < 10:
             y = y + 1
+            print(x, y)
+        while x < 10:
+            print(x)
 
 
 main()
