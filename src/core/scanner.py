fe3122427f9003f05b48f0b4d854c158c266b763
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-01-24 02:03:50 +0100

    Refactor code
    Refactored major code scanning function. Distributed different operations such as hierarchy setting, code folding and translation in their corresponding methods in order to enforce encapsulation.
    
    Also removed logger support since it seemed unnecessary due to the presence of tests.

--- src/core/scanner.py
+++ src/core/scanner.py
@@ -7,175 +7,125 @@ Authors:
 
 import regex as re
 
-from .block import Block, TYPES
-from .logger import Logger
+from .block import TYPES, Block
 
 
 class Scanner:
-    """Code scanning class.
+    """Code scanning and organization class.
 
     This class is responsible for scanning the code and finding the blocks
-    that make up the program. It also sets the hierarchy of the blocks.
+    that compose the program. It also sets the hierarchy of the blocks,
+    collapses its contents and translates them.
 
     Attributes:
         code (str): the code to be scanned.
         lines (list[str]): the code trimmed and split into lines.
-        logger (Logger): the logger instance.
         blocks (list[Block]): the list of blocks found in the code.
         roots (list[Block]): the list of root blocks found in the code.
-        BLOCK_TYPES (dict[str, tuple[str, str]]): the block types and their
-            delimiters.
     """
 
-    def __init__(self, code: str, log_level: int = 2) -> None:
+    def __init__(self, code: str) -> None:
         """Initialize a scanner instance.
 
         Args:
             code (str): the code to be scanned.
-            log_level (int): the log level to be used.
         """
         self.code = code
         self.lines = [line.strip() for line in code.splitlines()]
-        self.logger = Logger(log_level)
         self.blocks: list[Block] = []
         self.roots: list[Block] = []
 
     def scan(self) -> None:
-        """Scan the code and find the blocks.
+        """Scan the code and find structural blocks.
 
-        This method scans the code and finds the blocks that make up the
-        program. It also sets the hierarchy of the blocks.
+        This method is a public caller for the recursive `Scanner._scan`
+        method, which is responsible for iterating over the code and each
+        block type, identifying its header and footer and enclosing the body
+        in between.
+        """
+        self.blocks = self._scan(start=0)
+        self._organize()
+
+    def _scan(self, start: int) -> list[Block]:
+        """Scan the code and find structural blocks.
+
+        This method iterates over each line of code and every defined block
+        type, identifying the corresponding headers and footers with each line
+        of code and classifying the blocks accordingly.
+
+        Args:
+            start (int): the index from which the search begins.
+
+        Returns:
+            list[Block]: the list of identified `Block` elements.
+
+        Notes:
+            This method should not be called directly, since it is designed for
+            internal use and it might have undefined outputs if it is called
+            manually.
         """
         blocks = []
-        for block_type in TYPES:
-            i = 0
-            di = i + 1  # Displayed index.
 
-            self.logger.log(
-                f"Searching for {block_type.__name__} blocks...",
-                log_level=1
-            )
+        i = 0
+        while i < len(self.lines[start:]):
+            line = self.lines[start:][i]
 
-            while i < len(self.lines):
-                line = self.lines[i]
+            for block_type in TYPES:
+                header, footer = block_type.HEADER, block_type.FOOTER
 
-                self.logger.log(
-                    f"{di:0>3} - Analyzing \"{line}\"",
-                    log_level=0
-                )
+                if re.match(header, line, flags=block_type.FLAGS):
+                    blocks.extend(self._scan(start + i + 1))
 
-                if re.match(block_type.HEADER, line, flags=block_type.FLAGS):
-                    self.logger.log(
-                        f"{di:0>3} - Found header. Recursing...",
-                        log_level=1
-                    )
+                    if blocks:
+                        indices = {block.end: block for block in blocks}
+                        i = indices[max(indices)].end - start
 
-                    blocks.extend(
-                        self._find_blocks(
-                            self.lines,
-                            block_type,
-                            i + 1
+                if re.match(footer, line, flags=block_type.FLAGS):
+                    blocks.append(
+                        block_type(
+                            self.lines[start - 1:start + i + 1],
+                            start - 1,
+                            start + i
                         )
                     )
 
-                    self.logger.log(
-                        f"{di:0>3} - Returning to root...",
-                        log_level=1
-                    )
-
-                i += 1
-
-        self.blocks = list(set(blocks))
-        self._set_hierarchy()
-
-    def _find_blocks(self, lines: list[str],
-                     block_type, start: int) -> list[Block]:
-        """Identify blocks in the code.
-
-        This method is a recursive function that finds the blocks in the code
-        and returns them as a list.
-
-        Args:
-            lines (list[str]): the code trimmed and split into lines.
-            block_type (Block): the block type to search for.
-            start (int): the line to start searching from.
-        """
-        blocks = []
-        header, footer = block_type.HEADER, block_type.FOOTER
-
-        self.logger.log(
-            f"Searching for \"{header}\" or \"{footer}\" from line {start}",
-            log_level=0
-        )
-
-        i = 0
-        while i < len(lines[start:]):
-            di = i + 1  # Displayed index.
-            line = lines[start:][i]
-
-            self.logger.log(
-                f"({start + di:0>3}) {di:0>3} - Analyzing \"{line}\"",
-                log_level=0
-            )
-
-            if re.match(header, line, flags=block_type.FLAGS):
-                self.logger.log(
-                    f"({start + di:0>3}) {di:0>3} - Found header. "
-                    + "Recursing...",
-                    log_level=0
-                )
-
-                blocks.extend(
-                    self._find_blocks(
-                        lines,
-                        block_type,
-                        i + start + 1
-                    )
-                )
-
-                if blocks:
-                    i = max(blocks, key=lambda x: x.end).end - start
-                    self.logger.log(f"Detected blocks {blocks}", log_level=1)
-                else:
-                    self.logger.log("No blocks detected", log_level=2)
-
-            if re.match(footer, line, flags=block_type.FLAGS):
-                self.logger.log(
-                    f"({start + di:0>3}) {di:0>3} - Found footer. "
-                    + "Returning to previous call...",
-                    log_level=0
-                )
-
-                blocks.append(block_type(
-                    lines[start - 1:start + i + 1],
-                    start - 1,
-                    start + i
-                ))
-
-                return blocks
+                    return blocks
 
             i += 1
 
         return blocks
 
+    def _organize(self) -> None:
+        """Organize scanned blocks.
+
+        This method calls several other methods that set up the block
+        hierarchy, define the roots of the block tree, collapse nested blocks
+        and translate their contents.
+        """
+        self._set_hierarchy()
+        self._set_roots()
+        self._collapse()
+        self._translate()
+
     def _set_hierarchy(self) -> None:
         """Set the hierarchy of the blocks.
 
         This method sets the hierarchy of the blocks by setting the parent
-        and children attributes of each block.
+        and children attributes of each block based on the containment of some
+        blocks into others.
         """
         for block in self.blocks:
             remaining = [other for other in self.blocks if block in other]
 
             if remaining:
-                parent = min(
-                    remaining,
-                    key=lambda x: abs(x.start - block.start)
-                )
+                distances = {
+                    abs(other.start - block.start): other
+                    for other in remaining
+                }
+                parent = distances[min(distances)]
+
                 block.parent = parent
                 parent.children.append(block)
-
-        self._set_roots()
 
     def _set_roots(self) -> None:
         """Set the root blocks.
@@ -184,31 +134,34 @@ class Scanner:
         parent.
         """
         self.roots = sorted(
-            [block for block in self.blocks if block.is_root()])
+            [block for block in self.blocks if block.is_root()]
+        )
+
+    def _collapse(self) -> None:
+        """Collapse the contents of the blocks."""
         for root in sorted(self.roots):
             root.fold()
 
+    def _translate(self) -> None:
+        """Translate scanned blocks."""
         for block in sorted(self.blocks):
             block.translate()
 
-    def tree(self) -> str:
-        """Return the unrendered, indented representation of the blocks.
+    def render(self, collapsed: bool = False) -> str:
+        """Render the tree block representation.
+
+        Args:
+            collapsed (bool, optional): whether to render the block tree
+                collapsing the contents of each block or not. Defaults to
+                False.
 
         Returns:
-            str: the tree unrendered, indented representation of the blocks.
+            str: the rendered and indented tree block representation.
         """
-        output = ''
-        for root in self.roots:
-            output += root.tree()
-        return output.strip()
+        if collapsed:
+            return ''.join(root.tree() for root in self.roots).strip()
 
-    def render(self) -> str:
-        """Return the rendered, indented representation of the blocks.
-
-        Returns:
-            str: the rendered, indented representation of the blocks.
-        """
-        output = ''
-        for root in self.roots:
-            output += '\n'.join(root.render()) + '\n'
-        return output.strip()
+        return '\n'.join(
+            '\n'.join(root.render())
+            for root in self.roots
+        ).strip()
