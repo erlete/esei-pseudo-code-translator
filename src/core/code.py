390e9c4ab4c738afffe5ca2c0e28b67437e889da
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-01-25 01:15:10 +0100

    Reformat code
    Due to the nature of the `top` and `bottom` statements, they were renamed to `globals` and `calls`, respectively.
    
    Furthermore, added functionality to the `Code.render` method so that it updates global declarations on the `main` function definition.

--- src/core/code.py
+++ src/core/code.py
@@ -6,20 +6,20 @@ Authors:
 
 
 from .scanner import Scanner
+from .block import Main
 
 
 class Code:
     """Code organization class.
 
-    This class is responsible for organizing and rendering final code blocks.
-    It is required due to the fact that some code blocks require header or
-    footer statements, such as `global` declarations for variables or `main`
-    function calls at the bottom of the script.
+    This class is responsible for correctly assigning global declarations to
+    the main function, as well as defining function calls at the bottom of the
+    program.
 
     Attributes:
         scanner (Scanner): the scanner object that identifies blocks.
-        top (list[str]): global declarations at the top of the script.
-        bottom (list[str]): function calls at the bottom of the script.
+        globals (list[str]): global declarations at the top of the script.
+        calls (list[str]): function calls at the bottom of the script.
     """
 
     def __init__(self, scanner: Scanner):
@@ -29,20 +29,20 @@ class Code:
             scanner (Scanner): tje scanner object that identifies blocks.
         """
         self.scanner = scanner
-        self._set_top()
-        self._set_bottom()
+        self._set_globals()
+        self._set_calls()
 
-    def _set_top(self):
-        """Set top statements."""
-        self.top = []
+    def _set_globals(self):
+        """Set global statements."""
+        self.globals = []
         for block in self.scanner.blocks:
-            self.top.extend(block.top)
+            self.globals.extend(block.globals)
 
-    def _set_bottom(self):
-        """Set bottom statements."""
-        self.bottom = []
+    def _set_calls(self):
+        """Set call statements."""
+        self.calls = []
         for block in self.scanner.blocks:
-            self.bottom.extend(block.bottom)
+            self.calls.extend(block.calls)
 
     def render(self) -> str:
         """Render the final organized code.
@@ -50,15 +50,21 @@ class Code:
         Returns:
             str: the final organized code.
         """
-        if self.top:
-            header = f"global {', '.join(str(top) for top in self.top)}\n"
-        else:
-            header = ''
-
-        if self.bottom:
-            footer = '\n'.join(str(bottom) for bottom in self.bottom)
+        if self.calls:
+            footer = '\n'.join(str(bottom) for bottom in self.calls)
             footer = f"\n\n{footer}\n"
         else:
             footer = ''
 
-        return f"{header}{self.scanner.render()}{footer}"
+        main_index = None
+        for i, block in enumerate(self.scanner.blocks):
+            if isinstance(block, Main):
+                main_index = i
+                break
+
+        if main_index is not None and self.globals:
+            self.scanner.blocks[main_index]._body = [
+                f"global {', '.join(glob for glob in self.globals)}"
+            ] + self.scanner.blocks[main_index]._body
+
+        return f"{self.scanner.render()}{footer}"
